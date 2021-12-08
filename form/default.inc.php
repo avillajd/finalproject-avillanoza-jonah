175a39f5c42cdbe608e314030d7b117bf15a3441
Jonah Avillanoza <j.avillanoza@ufl.edu>
2021-12-07 21:40:06 -0500

    Biopage added and css added

---
+++ form/default.inc.php
@@ -0,0 +1,110 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+header {
+  padding: 8px;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 99;
+  background-color: white;
+  padding-bottom: inherit;
+}
+
+h1,
+nav {
+  display: inline-block;
+}
+
+nav {
+  float: right;
+  padding: 60px 150px 0px;
+}
+
+li,
+a {
+  color: #707070;
+  text-decoration: none;
+  transition: 0.5s;
+}
+
+a:hover {
+  color: orange;
+  text-decoration: underline;
+  transition: 0.5s;
+}
+
+.workpage {
+  color: orange;
+  text-decoration: underline;
+}
+
+ul,
+li {
+  display: inline-block;
+  font-family: helvetica;
+  color: #707070;
+  font-weight: normal;
+  font-size: 16px;
+  margin: 10px;
+}
+
+.boldname {
+  font-family: helvetica;
+  color: #707070;
+  position: relative;
+  top: 16px;
+}
+
+.design {
+  font-weight: normal;
+}
+
+.mainanchor,
+.boldname {
+  display: inline-block;
+}
+
+.mainanchor {
+  vertical-align: middle;
+  padding: 45px 5px 0px 45px;
+}
+
+.twitter,
+.share {
+  vertical-align: middle;
+}
+
+.container {
+  padding: 200px;
+  text-align: center;
+  font-family: helvetica;
+}
+
+</style>
+
+
+<header>
+    <img class="mainanchor" src="images/multicam.png" alt="rfsanchor" width="100">
+    <h1 class="boldname">Jonah Avillanoza | <span class="design">Design</span></h1>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="work.html">Work</a></li>
+            <li><a href="bio-contact.html">Bio/Contact</a></li>
+            <li><a href="music.html">Music</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="https://twitter.com/Ninety_Two_VB" target="blank"><img class="twitter" src="images/twitterlogo.png" alt="twitterlogo" width="30"></a></li>
+            <li><a href="https://www.instagram.com/riggedforsea" target="blank"><img class="share" src="images/share.png" alt="sharelogo" width="15"></a></li>    
+        </ul>
+    </nav>
+</header> 
+
+<div class="container">
+    <h1>Uh oh ! Something went wrong !</h1>
+    <p>please try again</p>
+</div>
